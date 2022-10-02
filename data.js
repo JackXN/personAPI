d5972addd01ea850693f34660f7bbcc619c87839
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-10-02 15:16:51 -0600

    people

--- data.js
+++ data.js
@@ -1,10 +1,11 @@
 import fetch from 'node-fetch';
+// import data from './db.json' assert {type:'json'};
 
-console.log(data)
 async function fetchData() {
-    let response = await fetch('./db.json')
-    // let data = await response.json()
-    // console.log(data)
+    let response = await fetch('https://my-json-server.typicode.com/jackxn/personAPI/results')
+    let data = await response.json()
+    console.log(data)
+    
     
 }
 
